b20ed20963afedd49e5a052ba34f607a7dddb7e3
71220850_yoseph sargio napa <112671264+NOID1622@users.noreply.github.com>
2024-04-18 13:21:39 +0700

    Update delete.php

--- TipeA/delete.php
+++ TipeA/delete.php
@@ -1,3 +1,15 @@
 <?php
     // kode anda
+    include "koneksi.php";
+    $sql = "DELETE FROM stockBarang WHERE idBarang ='".$_GET['id']."'";
+    $result = mysqli_query($conn, $sql);
+    if ($result){
+        echo "Berhasil Menhapus data";
+        header("Location: index.php");
+    }else{
+        echo "Gagal Menhapus Data";
+        
+    }
+    $conn->close();
+    
 ?>
